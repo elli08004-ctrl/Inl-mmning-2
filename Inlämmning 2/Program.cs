bb57d3a34ee9ca2750dbee62057ec523c2a7507d
elli08004 <elli08004@E-PW05SJ7T>
2025-09-03 13:00:18 +0200

    Add project files.

---
+++ Inlämmning 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift_1._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ojdå, det hände visst något.");
+            Console.ReadKey();
+        }
+    }
+}
